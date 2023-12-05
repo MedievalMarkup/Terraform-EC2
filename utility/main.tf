6f0ea0ec7ea9855fdeb53f9a2b08a79daf7c9f53
root <root@Adil-Nehal.ai.local>
2023-12-05 18:17:21 +0500

    adding utility to check available ec2s

---
+++ utility/main.tf
@@ -0,0 +1,14 @@
+data "aws_ec2_instance_type_offerings" "chk_az_for_ec2" {
+  filter {
+    name   = "instance-type"
+    //list of strng
+    values = var.aws_ec2_instance_type
+  }
+
+  filter {
+    name   = "location"
+    values = var.chk_ec2_loc
+  }
+
+  location_type = var.loc_type
+}
---
+++ utility/variables.tf
@@ -0,0 +1,17 @@
+variable "chk_ec2_loc" {
+  description = "location"
+  type        = list(string)
+  default     = ["us-west-2"]
+}
+
+variable "loc_type" {
+  description = "location type"
+  type        = list(string)
+  default     = ["availability-zone"]  
+}
+
+variable "aws_ec2_instance_type" {
+  description = "AWS EC2 Instance Type"
+  type = list(string)
+  default = ["t2.micro"]
+}
