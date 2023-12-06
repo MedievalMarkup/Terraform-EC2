d4718391166255667b1a193f37dba2f3c58cda8d
root <root@Adil-Nehal.ai.local>
2023-12-06 22:52:33 +0500

    initial

--- utility/main.tf
+++ utility/main.tf
@@ -1,5 +1,20 @@
+
+
+
+//to make the below working fully dynamic - taking help from av datasource
+data "aws_availability_zones" "az_for_ec2" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+
+
+
 data "aws_ec2_instance_type_offerings" "chk_az_for_ec2" {
-  for_each = toset(var.chk_ec2_loc)
+  //for_each = toset(var.chk_ec2_loc)
+  for_each = toset(data.aws_availability_zones.az_for_ec2.names)
   filter {
     name   = "instance-type"
     //list of strng
--- utility/output.tf
+++ utility/output.tf
@@ -1,4 +1,39 @@
+
+//first two output values are semi-dynamic version (manually adding the array of locations)
+
 output "chk_av_ec2_type" {
   //value = data.aws_ec2_instance_type_offerings.chk_az_for_ec2.instance_types
   value = [for t in data.aws_ec2_instance_type_offerings.chk_az_for_ec2: t.instance_types]
+}
+
+output "get_av_ec2_type" {
+  value = {
+     for az, details in data.aws_ec2_instance_type_offerings.chk_az_for_ec2: az => details.instance_types
+  }
+}
+
+
+//fully dynamic - getting from datasource, see main.tf
+
+//all azs mapped to supported instance type
+output "av_mapp_ec2_type" {
+  value = {
+     for az, details in data.aws_ec2_instance_type_offerings.chk_az_for_ec2: az => details.instance_types
+  }
+}
+
+//exclude unsupported azs
+output "av_mapp_ec2_type_exc" {
+  value = {
+     for az, details in data.aws_ec2_instance_type_offerings.chk_az_for_ec2: 
+     az => details.instance_types if length(details.instance_types) != 0
+  }
+}
+
+//exclude instances, only get keys that's mean location only
+output "av_mapp_ec2_type_exc_ec2_type" {
+  value = keys({
+     for az, details in data.aws_ec2_instance_type_offerings.chk_az_for_ec2:
+     az => details.instance_types if length(details.instance_types) != 0
+  })
 }
